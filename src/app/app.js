53c5de7705d3401bd54edfcfa48a13830c03c2b7
russi115 <shamanse2001@gmail.com>
2021-10-15 01:34:54 -0400

    Updates

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Nav from "./navegation";
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <Nav/>
+            </div>
+        )
+    }
+}
+
+export default App;
